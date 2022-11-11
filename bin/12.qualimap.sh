50b686d1cd86b7c1ce59b35332ea206efb20a39d
Isaura Rosas Reinhold <35971939+IsauraRReinhold@users.noreply.github.com>
2022-11-11 13:28:32 -0600

    Delete bin directory
    this was removed because i will add another

--- bin/12.qualimap.sh
+++
@@ -1,5 +0,0 @@
-
-
-
-
-qualimap multi-bamqc -d /media/cris/Isaura/prosopis_project/out/stats/data.txt -gff /media/cris/Isaura/prosopis_project/out/stats/PC_final_gene_all_function_stringtie.gtf  -outdir /media/cris/Isaura/prosopis_project/out/stats/  -outformat PDF -r
